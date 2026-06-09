ebc0957c7bcbf7960e3e5c43ae4ee4e4b2a8dbf7
Steven <steven24koning@gmail.com>
2026-06-09 22:39:26 +0200

    chore: remove old monolithic src and public assets

--- src/database/index.ts
+++
@@ -1,9 +0,0 @@
-import {Database} from "bun:sqlite";
-import * as path from "path";
-
-const dbPath = path.join(import.meta.dir, "../..", "sbase.db");
-const db = new Database(dbPath);
-
-console.log(`Connected to SQLite database at: ${dbPath}`);
-
-export default db;
--- src/database/seeders/000_module_seeder.sql
+++
@@ -1,3 +0,0 @@
---seed recipe module
-INSERT INTO modules (module_name, module_alias, description)
-VALUES ('recipes', 'Taste tracker', 'Module for managing recipes');
